57ad2eb3ddbb6af11e6538c44ff2b90cd40151f5
ben <{username}@users.noreply.github.com>
2024-01-21 20:18:54 -0600

    configure swagger

--- Program.cs
+++ Program.cs
@@ -1,18 +1,31 @@
 
 namespace pihole_api
 {
+    using System.Reflection;
+
     public class Program
     {
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            // Logging
+            builder.Logging.ClearProviders();
+            builder.Logging.AddConsole();
+
             // Add services to the container.
 
             builder.Services.AddControllers();
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFile));
+            });
+
+
 
             var app = builder.Build();
 
